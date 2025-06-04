2da71dc606bc951feab9f64cdc08c62c2919a6a6
Rengga <renggareznatra.daniswara@gmail.comgit config --global username RenggaReznatra123>
2025-06-04 11:10:17 +0700

    feat: [RENGGA] Add List Pemain

--- app/src/main/java/com/example/pas_absen28_absen30/APIservise.java
+++ app/src/main/java/com/example/pas_absen28_absen30/APIservise.java
@@ -8,6 +8,7 @@ public interface APIservise {
     Call<TeamResponse> getEnglishPremierleague(@Query("l") String league);
 
     @GET("/api/v1/json/3/eventsround.php?id=4335&r=36&s=2024-2025")
-    Call<JadwalResponse> getJadwalLaliga(@Query("j") String jadwal1);
+    Call<JadwalResponse> getJadwalLaliga();
+
 
 }
